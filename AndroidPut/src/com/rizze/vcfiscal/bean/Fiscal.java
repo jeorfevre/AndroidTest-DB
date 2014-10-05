2b1d0b5ba6efa0ee40e065f37c32fd047fe9e13e
jeorfevre <jean-emmanuel.orfevre@rizze.com>
2014-10-05 05:15:05 -0300

    0.0.2

--- AndroidPut/src/com/rizze/vcfiscal/bean/Fiscal.java
+++ AndroidPut/src/com/rizze/vcfiscal/bean/Fiscal.java
@@ -2,7 +2,7 @@ package com.rizze.vcfiscal.bean;
 
 import com.google.gson.annotations.Expose;
 
-public class Fiscal{ 					 //JECONEX		
+public class Fiscal{ 					 
 	@Expose public String sobrenome;
 	@Expose public String nome;
 	@Expose public String email;
